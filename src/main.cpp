ab6aa5e531affb540e97fefc039bd5e5ac360434
yulivee <coding@voiding-warranties.de>
2017-01-27 23:00:41 +0100

    minimal version, basic control

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include <Arduino.h>
-int links1 = 6;
-int rechts1 = 5;
-int enable1 = 4;
-int links2 = 9;
-int rechts2 = 10;
-int enable2 = 7;
-int rechts3 = ;
-int links3 = ;
+int links_ske = 5;
+int rechts_ske = 4;
+int links_skf = 2;
+int rechts_skf = 3;
+int links_skg = 12;
+int rechts_skg = 13;
+int links_uaj = 7;
+int rechts_uaj = 6 ;
+int links_uae = 9;
+int rechts_uae = 8;
+int links_shoulder = 10;
+int rechts_shoulder = 11;
 int monitor = 0;
 
 int tempo = 100;
@@ -14,92 +18,153 @@ int tempo = 100;
 void setup()
 {
     Serial.begin(9600);
-    pinMode(enable1, OUTPUT);
-    digitalWrite(enable1, HIGH);
-    pinMode(enable2, OUTPUT);
-    digitalWrite(enable2, HIGH);
-    pinMode(links1, OUTPUT);
-    pinMode(rechts1, OUTPUT);
-    pinMode(links2, OUTPUT);
-    pinMode(rechts2, OUTPUT);
+    //pinMode(enable1, OUTPUT);
+    //digitalWrite(enable1, HIGH);
+    pinMode(links_shoulder, OUTPUT);
+    pinMode(rechts_shoulder, OUTPUT);
+    pinMode(links_uae, OUTPUT);
+    pinMode(rechts_uae, OUTPUT);
+    pinMode(links_uaj, OUTPUT);
+    pinMode(rechts_uaj, OUTPUT);
+    pinMode(links_skg, OUTPUT);
+    pinMode(rechts_skg, OUTPUT);
+    pinMode(links_skf, OUTPUT);
+    pinMode(rechts_skf, OUTPUT);
+    pinMode(links_ske, OUTPUT);
+    pinMode(rechts_ske, OUTPUT);
 
-
-    analogWrite(links1, 0);
-    analogWrite(rechts1, 0);
-    analogWrite(links2, 0);
-    analogWrite(rechts2, 0);
+    analogWrite(links_shoulder, 0);
+    analogWrite(rechts_shoulder, 0);
+    analogWrite(links_uae, 0);
+    analogWrite(rechts_uae, 0);
+    analogWrite(links_uaj, 0);
+    analogWrite(rechts_uaj, 0);
+    analogWrite(links_skg, 0);
+    analogWrite(rechts_skg, 0);
+    analogWrite(links_skf, 0);
+    analogWrite(rechts_skf, 0);
+    analogWrite(links_ske, 0);
+    analogWrite(rechts_ske, 0);
 }
 void stop_motors () {
-    analogWrite(links1, 0);
-    analogWrite(rechts1, 0);
-    analogWrite(links2, 0);
-    analogWrite(rechts2, 0);
+    analogWrite(links_shoulder, 0);
+    analogWrite(rechts_shoulder, 0);
+    analogWrite(links_uae, 0);
+    analogWrite(rechts_uae, 0);
+    analogWrite(links_uaj, 0);
+    analogWrite(rechts_uaj, 0);
+    analogWrite(links_skg, 0);
+    analogWrite(rechts_skg, 0);
+    analogWrite(links_skf, 0);
+    analogWrite(rechts_skf, 0);
+    analogWrite(links_ske, 0);
+    analogWrite(rechts_ske, 0);
 }
+
 void move_shoulder_right () {
-    analogWrite(links1, tempo);
-    analogWrite(rechts1, 0 );
+    analogWrite(links_shoulder, tempo);
+    analogWrite(rechts_shoulder, 0 );
     delay(1000);
     stop_motors();
 }
+
 void move_shoulder_left () {
-    analogWrite(links1, 0);
-    analogWrite(rechts1, tempo );
+    analogWrite(links_shoulder, 0);
+    analogWrite(rechts_shoulder, tempo );
     delay(1000);
     stop_motors();
 }
+
 void move_uae_right () {
-    analogWrite(links2,tempo);
-    analogWrite(rechts2, 0);
+    analogWrite(links_uae,tempo);
+    analogWrite(rechts_uae, 0);
     delay(1000);
     stop_motors();
 }
+
 void move_uae_left () {
-    analogWrite(links2, 0);
-    analogWrite(rechts2, tempo);
+    analogWrite(links_uae, 0);
+    analogWrite(rechts_uae, tempo);
     delay(1000);
     stop_motors();
 }
 
 void move_uaj_right () {
-    analogWrite(links3,tempo);
-    analogWrite(rechts3, 0);
+    analogWrite(links_uaj,tempo);
+    analogWrite(rechts_uaj, 0);
     delay(1000);
     stop_motors();
 }
+
 void move_uaj_left () {
-    analogWrite(links3, 0);
-    analogWrite(rechts3, tempo);
+    analogWrite(links_uaj, 0);
+    analogWrite(rechts_uaj, tempo);
     delay(1000);
     stop_motors();
 }
+
+void move_ske_right () {
+    analogWrite(links_ske,tempo);
+    analogWrite(rechts_ske, 0);
+    delay(1000);
+    stop_motors();
+}
+
+void move_ske_left () {
+    analogWrite(links_ske, 0);
+    analogWrite(rechts_ske, tempo);
+    delay(1000);
+    stop_motors();
+}
+
+void move_skg_right () {
+    analogWrite(links_skg,tempo);
+    analogWrite(rechts_skg, 0);
+    delay(1000);
+    stop_motors();
+}
+
+void move_skg_left () {
+    analogWrite(links_skg, 0);
+    analogWrite(rechts_skg, tempo);
+    delay(1000);
+    stop_motors();
+}
+
+void move_skf_right () {
+    analogWrite(links_skf,tempo);
+    analogWrite(rechts_skf, 0);
+    delay(1000);
+    stop_motors();
+}
+
+void move_skf_left () {
+    analogWrite(links_skf, 0);
+    analogWrite(rechts_skf, tempo);
+    delay(1000);
+    stop_motors();
+}
+
 void loop()
 {
 
     if (Serial.available() > 0) {
         monitor = Serial.read();
 
-        if(monitor == 49) { // aus
+        if( monitor == 49 ) { // aus
             stop_motors();
-            //analogWrite(links1, 0);
-            //analogWrite(rechts1, 0);
-            //analogWrite(links2, 0);
-            //analogWrite(rechts2, 0);
         }
 
-        if(monitor == 113) {
-//   analogWrite(links1, tempo);
-//   analogWrite(rechts1, 0);
+        if( monitor == 113 ) {
             move_shoulder_right();
             monitor = 49;
         }
 
-        if(monitor == 119) {
-//   analogWrite(links1, 0);
-//   analogWrite(rechts1, tempo);
+        if( monitor == 119 ) {
             move_shoulder_left();
             monitor = 49;
         }
-        if( monitor == 97) {
+        if( monitor == 97 ) {
             move_uae_left();
             monitor = 49;
         }
@@ -114,11 +179,35 @@ void loop()
 
         }
         if ( monitor == 120 ) {
-            move_uaj_right();
+            move_uaj_left();
             monitor = 49;
 
         }
-        Serial.println(monitor);
+	if ( monitor == 101 ) {
+	    move_ske_right();
+            monitor = 49;
+	}
+	if ( monitor == 114 ) {
+	    move_ske_left();
+            monitor = 49;
+	}
+	if ( monitor == 100 ) {
+	    move_skf_right();
+            monitor = 49;
+	}
+	if ( monitor == 102 ) {
+	    move_skf_left();
+            monitor = 49;
+	}
+	if ( monitor == 99 ) {
+	    move_skg_right();
+            monitor = 49;
+	}
+	if ( monitor == 118 ) {
+	    move_skg_left();
+            monitor = 49;
+	}
+        Serial.println( monitor );
     }
     delay(100);
 }
