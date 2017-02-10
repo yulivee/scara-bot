5b758ce29e607155d7f45a057de8e870ce4f2ec8
yulivee <coding@voiding-warranties.de>
2017-02-10 19:25:27 +0100

    rebuild to pins.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,6 @@
 #include <Arduino.h>
-int links_ske = 5;
-int rechts_ske = 4;
-int links_skf = 2;
-int rechts_skf = 3;
-int links_skg = 12;
-int rechts_skg = 13;
-int links_uaj = 7;
-int rechts_uaj = 6 ;
-int links_uae = 9;
-int rechts_uae = 8;
-int links_shoulder = 10;
-int rechts_shoulder = 11;
+#include "pins.h"
+
 int monitor = 0;
 
 int tempo = 100;
@@ -18,129 +8,127 @@ int tempo = 100;
 void setup()
 {
     Serial.begin(9600);
-    //pinMode(enable1, OUTPUT);
-    //digitalWrite(enable1, HIGH);
-    pinMode(links_shoulder, OUTPUT);
-    pinMode(rechts_shoulder, OUTPUT);
-    pinMode(links_uae, OUTPUT);
-    pinMode(rechts_uae, OUTPUT);
-    pinMode(links_uaj, OUTPUT);
-    pinMode(rechts_uaj, OUTPUT);
-    pinMode(links_skg, OUTPUT);
-    pinMode(rechts_skg, OUTPUT);
-    pinMode(links_skf, OUTPUT);
-    pinMode(rechts_skf, OUTPUT);
-    pinMode(links_ske, OUTPUT);
-    pinMode(rechts_ske, OUTPUT);
+    pinMode(SHOULDER_LEFT, OUTPUT);
+    pinMode(SHOULDER_RIGHT, OUTPUT);
+    pinMode(UAE_LEFT, OUTPUT);
+    pinMode(UAE_RIGHT, OUTPUT);
+    pinMode(UAJ_LEFT, OUTPUT);
+    pinMode(UAJ_RIGHT, OUTPUT);
+    pinMode(SKG_LEFT, OUTPUT);
+    pinMode(SKG_RIGHT, OUTPUT);
+    pinMode(SKF_LEFT, OUTPUT);
+    pinMode(SKF_RIGHT, OUTPUT);
+    pinMode(SKE_LEFT, OUTPUT);
+    pinMode(SKE_RIGHT, OUTPUT);
 
-    analogWrite(links_shoulder, 0);
-    analogWrite(rechts_shoulder, 0);
-    analogWrite(links_uae, 0);
-    analogWrite(rechts_uae, 0);
-    analogWrite(links_uaj, 0);
-    analogWrite(rechts_uaj, 0);
-    analogWrite(links_skg, 0);
-    analogWrite(rechts_skg, 0);
-    analogWrite(links_skf, 0);
-    analogWrite(rechts_skf, 0);
-    analogWrite(links_ske, 0);
-    analogWrite(rechts_ske, 0);
+    analogWrite(SHOULDER_LEFT, 0);
+    analogWrite(SHOULDER_RIGHT, 0);
+    analogWrite(UAE_LEFT, 0);
+    analogWrite(UAE_RIGHT, 0);
+    analogWrite(UAJ_LEFT, 0);
+    analogWrite(UAJ_RIGHT, 0);
+    analogWrite(SKG_LEFT, 0);
+    analogWrite(SKG_RIGHT, 0);
+    analogWrite(SKF_LEFT, 0);
+    analogWrite(SKF_RIGHT, 0);
+    analogWrite(SKE_LEFT, 0);
+    analogWrite(SKE_RIGHT, 0);
 }
 void stop_motors () {
-    analogWrite(links_shoulder, 0);
-    analogWrite(rechts_shoulder, 0);
-    analogWrite(links_uae, 0);
-    analogWrite(rechts_uae, 0);
-    analogWrite(links_uaj, 0);
-    analogWrite(rechts_uaj, 0);
-    analogWrite(links_skg, 0);
-    analogWrite(rechts_skg, 0);
-    analogWrite(links_skf, 0);
-    analogWrite(rechts_skf, 0);
-    analogWrite(links_ske, 0);
-    analogWrite(rechts_ske, 0);
+    analogWrite(SHOULDER_LEFT, 0);
+    analogWrite(SHOULDER_RIGHT, 0);
+    analogWrite(UAE_LEFT, 0);
+    analogWrite(UAE_RIGHT, 0);
+    analogWrite(UAJ_LEFT, 0);
+    analogWrite(UAJ_RIGHT, 0);
+    analogWrite(SKG_LEFT, 0);
+    analogWrite(SKG_RIGHT, 0);
+    analogWrite(SKF_LEFT, 0);
+    analogWrite(SKF_RIGHT, 0);
+    analogWrite(SKE_LEFT, 0);
+    analogWrite(SKE_RIGHT, 0);
 }
 
 void move_shoulder_right () {
-    analogWrite(links_shoulder, tempo);
-    analogWrite(rechts_shoulder, 0 );
+    analogWrite(SHOULDER_LEFT, tempo);
+    analogWrite(SHOULDER_RIGHT, 0 );
     delay(1000);
     stop_motors();
 }
 
 void move_shoulder_left () {
-    analogWrite(links_shoulder, 0);
-    analogWrite(rechts_shoulder, tempo );
+    analogWrite(SHOULDER_LEFT, 0);
+    analogWrite(SHOULDER_RIGHT, tempo );
     delay(1000);
     stop_motors();
 }
 
 void move_uae_right () {
-    analogWrite(links_uae,tempo);
-    analogWrite(rechts_uae, 0);
+    analogWrite(UAE_LEFT,tempo);
+    analogWrite(UAE_RIGHT, 0);
     delay(1000);
     stop_motors();
 }
 
 void move_uae_left () {
-    analogWrite(links_uae, 0);
-    analogWrite(rechts_uae, tempo);
+    analogWrite(UAE_LEFT, 0);
+    analogWrite(UAE_RIGHT, tempo);
     delay(1000);
     stop_motors();
 }
 
 void move_uaj_right () {
-    analogWrite(links_uaj,tempo);
-    analogWrite(rechts_uaj, 0);
+    analogWrite(UAJ_LEFT,tempo);
+    analogWrite(UAJ_RIGHT, 0);
     delay(1000);
     stop_motors();
 }
 
 void move_uaj_left () {
-    analogWrite(links_uaj, 0);
-    analogWrite(rechts_uaj, tempo);
+    analogWrite(UAJ_LEFT, 0);
+    analogWrite(UAJ_RIGHT, tempo);
     delay(1000);
     stop_motors();
 }
 
 void move_ske_right () {
-    analogWrite(links_ske,tempo);
-    analogWrite(rechts_ske, 0);
+    analogWrite(SKE_LEFT,tempo);
+    analogWrite(SKE_RIGHT, 0);
     delay(1000);
     stop_motors();
 }
 
 void move_ske_left () {
-    analogWrite(links_ske, 0);
-    analogWrite(rechts_ske, tempo);
+    analogWrite(SKE_LEFT, 0);
+    analogWrite(SKE_RIGHT, tempo);
     delay(1000);
     stop_motors();
 }
 
 void move_skg_right () {
-    analogWrite(links_skg,tempo);
-    analogWrite(rechts_skg, 0);
+    analogWrite(SKG_LEFT,tempo);
+    analogWrite(SKG_RIGHT, 0);
     delay(1000);
     stop_motors();
 }
 
 void move_skg_left () {
-    analogWrite(links_skg, 0);
-    analogWrite(rechts_skg, tempo);
+    analogWrite(SKG_LEFT, 0);
+    analogWrite(SKG_RIGHT, tempo);
     delay(1000);
     stop_motors();
 }
 
 void move_skf_right () {
-    analogWrite(links_skf,tempo);
-    analogWrite(rechts_skf, 0);
+    analogWrite(SKF_LEFT,tempo);
+    analogWrite(SKF_RIGHT, 0);
     delay(1000);
     stop_motors();
 }
 
 void move_skf_left () {
-    analogWrite(links_skf, 0);
-    analogWrite(rechts_skf, tempo);
+    analogWrite(SKF_LEFT, 0);
+    analogWrite(SKF_RIGHT, tempo);
     delay(1000);
     stop_motors();
 }
