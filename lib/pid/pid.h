c11f63ee394164b6b1947bedaae4a4f67ea85dca
yulivee <coding@voiding-warranties.de>
2017-05-06 20:11:40 +0200

    pid works. interrupt on rising AND falling interrupts for better resolution next time

--- lib/pid/pid.h
+++ lib/pid/pid.h
@@ -6,11 +6,11 @@
 extern "C" {
 #endif
 
-#define MOVEP 20 //P Motorspannung pro Schritt - Genauigkeit. Erzeugt Zittern und schießt übers Ziel raus
-#define MOVEI 2 //I Erhöht Motorspannung bei kleinen Fehlern - lässt die letzten bisschen Fehler verschwinden
-#define MOVED 20 //D Rechtzeitig bremsen - dämpft Zittern und rauschießen
+#define MOVEP 15 //P Motorspannung pro Schritt - Genauigkeit. Erzeugt Zittern und schießt übers Ziel raus
+#define MOVEI 0.5 //I Erhöht Motorspannung bei kleinen Fehlern - lässt die letzten bisschen Fehler verschwinden
+#define MOVED 10 //D Rechtzeitig bremsen - dämpft Zittern und rauschießen
 #define MOVEMAX 255
-#define MOVEIMAX 200    
+#define MOVEIMAX 100   
 #define GOVERNOR_FREQ 100	
 #define BOUNDS(var, max) if ((var)>(max)) (var)=(max); if ( (var)<-(max)) (var)=-(max);
     
