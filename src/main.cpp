d0005f4f4601c5eacef8b37bd17c056c54744895
Sandra Schuhmacher <coding@voiding-warranties.de>
2017-05-08 20:57:44 +0000

    astyled files

--- src/main.cpp
+++ src/main.cpp
@@ -10,25 +10,25 @@ volatile struct counts motor_cnt = { 0, 0 };
 volatile int positionDelta, positionSpeed, positionLastDelta, positionDiff, positionInt = 0;
 volatile int target_position = 0;
 volatile int current_position = 0;
-volatile int flag_0 = 0;                                                                                                         
-volatile int flag_1 = 0;                                                                                                         
+volatile int flag_0 = 0;
+volatile int flag_1 = 0;
 
-void doCount0() {                                                                                                            
-   flag_0 = digitalRead(motor_pins.cnt0);                                                                                     
-   flag_1 = digitalRead(motor_pins.cnt1);                                                                                     
-                                                                                                                                          
-   if ( flag_0 == flag_1 ) {                                                                                                       
-      motor_cnt.cnt0++;                                                                                                    
-   } else {                                                                                                                 
-      motor_cnt.cnt0--;                                                                                                    
-   } 
-} 
+void count_encoder() {
+    flag_0 = digitalRead(motor_pins.cnt0);
+    flag_1 = digitalRead(motor_pins.cnt1);
+
+    if ( flag_0 == flag_1 ) {
+        motor_cnt.cnt0++;
+    } else {
+        motor_cnt.cnt0--;
+    }
+}
 
 void setup()
 {
     Serial.begin(115200);
-    Serial.println("Scara-Bot ready"); 
-    attachInterrupt(digitalPinToInterrupt(motor_pins.cnt0), doCount0, CHANGE);
+    Serial.println("Scara-Bot ready");
+    attachInterrupt(digitalPinToInterrupt(motor_pins.cnt0), count_encoder(), CHANGE);
     timer_init();
     pinMode(motor_pins.cnt0, INPUT);
     pinMode(motor_pins.cnt1, INPUT);
@@ -44,36 +44,35 @@ void setup()
 void loop()
 {
 
-     monitor = Serial.read();
-	 Serial.print("Target: ");
-	 Serial.println(target_position);
-	 Serial.print("Current: ");
-	 Serial.println(motor_cnt.cnt0);
-	 Serial.print("Speed: ");
-	 Serial.println(positionSpeed);
-	 Serial.println("-------------");
+    monitor = Serial.read();
+    Serial.print("Target: ");
+    Serial.println(target_position);
+    Serial.print("Current: ");
+    Serial.println(motor_cnt.cnt0);
+    Serial.print("Speed: ");
+    Serial.println(positionSpeed);
+    Serial.println("-------------");
 
-        if( monitor == 49 ) { // aus
-	    digitalWrite(motor_pins.left, 0);
-	    digitalWrite(motor_pins.right, 0);
-        }
-        if ( monitor == 121 ) {
-	    target_position += clicks;
-        }
-        if ( monitor == 120 ) {
-	    target_position -= clicks;
-        }
-        if ( monitor == 107 ) {
-            clicks-=5;
-            Serial.print ("Anzahl Schritte: ");
-            Serial.println( clicks );
-        }
-        if ( monitor == 108 ) {
-            clicks+=5;
-            Serial.print ("Anzahl Schritte: ");
-            Serial.println( clicks );
-        }
-        //Serial.println( monitor );
+    if( monitor == 49 ) { // aus
+        digitalWrite(motor_pins.left, 0);
+        digitalWrite(motor_pins.right, 0);
+    }
+    if ( monitor == 121 ) {
+        target_position += clicks;
+    }
+    if ( monitor == 120 ) {
+        target_position -= clicks;
+    }
+    if ( monitor == 107 ) {
+        clicks-=5;
+        Serial.print ("Anzahl Schritte: ");
+        Serial.println( clicks );
+    }
+    if ( monitor == 108 ) {
+        clicks+=5;
+        Serial.print ("Anzahl Schritte: ");
+        Serial.println( clicks );
+    }
 
     _delay_ms(500);
 }
