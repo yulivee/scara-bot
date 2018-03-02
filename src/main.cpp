a0c09305c9aeb8b4da21d715f19b5fa85a448f1b
Daniel Veeckman <dav.veeckman@gmail.com>
2018-03-02 21:57:41 +0100

    Commentierung und Doku
    Einfügen von Commentaren und überarbeiten der Dokumentation

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <avr/delay.h>
 #include <pid.h>
 #include <pins.h>
+
+// VARIABLEN
 int monitor = 0;
 int clicks = 300;
 
@@ -13,10 +15,13 @@ volatile int current_position = 0;
 volatile int flag_0 = 0;
 volatile int flag_1 = 0;
 
+//FUNKTIONEN
+//Interruptfunktion zum Auswerten des Encoders
 void count_encoder() {
     flag_0 = digitalRead(motor_pins.cnt0);
     flag_1 = digitalRead(motor_pins.cnt1);
 
+    //Durch Vergleich der beiden Lichtschranken kann die Drehrichtung ermittelt werden.
     if ( flag_0 == flag_1 ) {
         motor_cnt.cnt0++;
     } else {
@@ -24,27 +29,34 @@ void count_encoder() {
     }
 }
 
+//INITIALISIERUNG
 void setup()
 {
+    //Starten der Seriellen Schnittstelle
     Serial.begin(115200);
-    Serial.println("Scara-Bot ready");
+    Serial.println("Chili-Bot ready");
+    //Deklarienen des Interrupts für den Motorencoder
     attachInterrupt(digitalPinToInterrupt(motor_pins.cnt0), count_encoder(), CHANGE);
+    //Starten des Timers für Delay-Funktionen
     timer_init();
+    //Initialisren der Pins
     pinMode(motor_pins.cnt0, INPUT);
     pinMode(motor_pins.cnt1, INPUT);
     pinMode(motor_pins.enable, OUTPUT);
     pinMode(motor_pins.left, OUTPUT);
     pinMode(motor_pins.right, OUTPUT);
-
+    //Ansteuern der Motorcontroller
     digitalWrite(motor_pins.left, 0);
     digitalWrite(motor_pins.right, 0);
     digitalWrite(motor_pins.enable, 1);
 }
 
+//HAUPTPROGRAMMSCHLEIFE
 void loop()
 {
-
+    //Auslesen eines Tastendrucks und ausgeben
     monitor = Serial.read();
+    //Ausgeben des Sollwertes
     Serial.print("Target: ");
     Serial.println(target_position);
     Serial.print("Current: ");
@@ -53,22 +65,23 @@ void loop()
     Serial.println(positionSpeed);
     Serial.println("-------------");
 
-    if( monitor == 49 ) { // aus
+    //Auswerten des Tasteneigabe
+    if( monitor == 49 ) { //1 - Motor aus
         digitalWrite(motor_pins.left, 0);
         digitalWrite(motor_pins.right, 0);
     }
-    if ( monitor == 121 ) {
+    if ( monitor == 121 ) { //y - Sollwert um Schrittgröße erhöhen
         target_position += clicks;
     }
-    if ( monitor == 120 ) {
+    if ( monitor == 120 ) { //x - Sollwert um Schrittgröße verringern
         target_position -= clicks;
     }
-    if ( monitor == 107 ) {
+    if ( monitor == 107 ) { //k - Schrittgröße verringern
         clicks-=5;
         Serial.print ("Anzahl Schritte: ");
         Serial.println( clicks );
     }
-    if ( monitor == 108 ) {
+    if ( monitor == 108 ) { //l - Schrittgröße erhöhen
         clicks+=5;
         Serial.print ("Anzahl Schritte: ");
         Serial.println( clicks );
