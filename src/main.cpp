7c84234b20064e8a7b34dc8673612cc279d722a3
Daniel Veeckman <dav.veeckman@gmail.com>
2018-09-02 19:11:48 +0200

    Serial-Read umgesetzt

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ volatile int pin_write = 7; //tells slave to read next position (from serial) or
 volatile int pin_prime = 8; //lets slave use the serial bus
 
 volatile int next_position = 0;
-
+volatile bool pin_toggled_high = true;
 
 // commented out for use as templae for needed functions
 //
@@ -46,84 +46,88 @@ volatile int next_position = 0;
 
 //count encoder steps function for interrupt
 void count_encoder(){
-    flag_0 = digitalRead(motor_pins.cnt0);
-    flag_1 = digitalRead(motor_pins.cnt1);
-    if ( flag_0 == flag_1 ) {
-        motor_cnt++;
-    } else {
-        motor_cnt--;
-    }
+  flag_0 = digitalRead(motor_pins.cnt0);
+  flag_1 = digitalRead(motor_pins.cnt1);
+  if ( flag_0 == flag_1 ) {
+    motor_cnt++;
+  } else {
+    motor_cnt--;
+  }
 }
 
 //clear serial input buffer
 void serial_clear(){
   while (Serial.available() > 0) {
-      Serial.read();
+    Serial.read();
   }
 }
 
-// int serial_read_int(){
-//   int byte_buffer[4];
-//   int conv_integer;
-//   Serial.readBytes(byte_buffer,4); //Read the serial data and store in var
-//   //convert buffer to conv_integer
-//   //TODO
-//   conv_integer = 0; //somthing to avoid errors
-//   return integer;
-// }
+int serial_read_2b_int(){
+  byte byte_buffer[2];
+  Serial.readBytes(byte_buffer,2); //Store the next 2 Bytes of serial data in the buffer
+  //convert buffer to conv_integer
+  int val = ((int)(byte_buffer[0]) << 8) + byte_buffer[1];
+  return val;
+}
 
 void setup(){
-    //Set the Serialport to 9600 Baud (other Bauds are possible, up to 115200)
-    Serial.begin(9600);
+  //Set the Serialport to 9600 Baud (other Bauds are possible, up to 115200)
+  Serial.begin(9600);
 
-    //start interrupt for counting the encoder steps
-    attachInterrupt(digitalPinToInterrupt(motor_pins.cnt0), count_encoder, CHANGE);
+  //start interrupt for counting the encoder steps
+  attachInterrupt(digitalPinToInterrupt(motor_pins.cnt0), count_encoder, CHANGE);
 
-    //start timer interrupt for PID controller
-    timer_init();
+  //start timer interrupt for PID controller
+  timer_init();
 
-    //initialise pins
-    pinMode(motor_pins.cnt0, INPUT);
-    pinMode(motor_pins.cnt1, INPUT);
-    pinMode(motor_pins.enable, OUTPUT);
-    pinMode(motor_pins.left, OUTPUT);
-    pinMode(motor_pins.right, OUTPUT);
-    pinMode(pin_hold, INPUT);
-    pinMode(pin_fire, INPUT);
-    pinMode(pin_write, INPUT);
-    pinMode(pin_prime, INPUT);
+  //initialise pins
+  pinMode(motor_pins.cnt0, INPUT);
+  pinMode(motor_pins.cnt1, INPUT);
+  pinMode(motor_pins.enable, OUTPUT);
+  pinMode(motor_pins.left, OUTPUT);
+  pinMode(motor_pins.right, OUTPUT);
+  pinMode(pin_hold, INPUT);
+  pinMode(pin_fire, INPUT);
+  pinMode(pin_write, INPUT);
+  pinMode(pin_prime, INPUT);
 
-
-    digitalWrite(motor_pins.left, 0);
-    digitalWrite(motor_pins.right, 0);
-    digitalWrite(motor_pins.enable, 1);
-
-    //Home
-    //Some code to make sure robot is in Home Position and all encoders are 0
-    //TODO
+  //make sure all motors are off, activate drivers are active
+  digitalWrite(motor_pins.left, 0);
+  digitalWrite(motor_pins.right, 0);
+  digitalWrite(motor_pins.enable, 1);
 }
 
 void loop()
 {
-    if (digitalRead(pin_prime)){
-      if (digitalRead(pin_write)) {
+  if (digitalRead(pin_prime)){  //prime signal tells Slave to send or receive data on the serial bus
+    if (pin_toggled_high) {     //check that sigal has bee toggles from off to on
+      pin_toggled_high = false;
+
+      if (digitalRead(pin_write)) {  //write to bus
+        Serial.write(motor_cnt);
+      }
+      else{//read from bus
         //clear serial buffer
         serial_clear();
         //send ready byte
         Serial.write(255);
-        //read next position from serial (4 bytes)
-        next_position = Serial.read(); //placeholder, leter the function int serial_read_int() shold be called
-      }else{
-        Serial.write(motor_cnt);
+        //read next position from serial (2 bytes)
+        next_position = serial_read_2b_int();
       }
     }
+  }
+  else{ //reset variable for recognisingoff-on toggles
+    pin_toggled_high = true;
+  }
 
-    if (!digitalRead(pin_hold)) {
-      //deactivate PID
-      //TODO
-    }
 
-    if (digitalRead(pin_fire)) {
-      target_position = next_position;
-    }
+  if (!digitalRead(pin_hold)) {
+    //deactivate motors
+    digitalWrite(motor_pins.enable, 1);
+    target_position = motor_cnt;
+  }
+
+  if (digitalRead(pin_fire)) {
+    target_position = next_position;
+  }
 }
